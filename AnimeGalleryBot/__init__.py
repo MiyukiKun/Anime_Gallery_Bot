03e3a94d4b4bb20c9fcf4984032c75402ac0e7ee
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-05-23 02:45:41 +0530

    Delete AnimeGalleryBot directory

--- AnimeGalleryBot/__init__.py
+++
@@ -1,8 +0,0 @@
-from telethon import TelegramClient
-import os
-
-api_id = os.environ.get('API_ID')
-api_hash = os.environ.get('API_HASH')
-bot_token = os.environ.get('BOT_TOKEN')
-
-tg_client = TelegramClient('bot1', api_id, api_hash).start(bot_token=bot_token)
