b39feca463ae30b31d86c7a8909101ad52c51d91
MiyukiKun <81800569+MiyukiKun@users.noreply.github.com>
2021-05-22 18:35:09 +0530

    Update __init__.py

--- AnimeGalleryBot/__init__.py
+++ AnimeGalleryBot/__init__.py
@@ -3,6 +3,6 @@ import os
 
 api_id = os.environ.get('API_ID')
 api_hash = os.environ.get('API_HASH')
-bot_token = '1877135847:AAEcmkyilJlG5hhE2IP6JWX23K8E6hybMIs'
+bot_token = os.environ.get('API_HASH')
 
 tg_client = TelegramClient('bot1', api_id, api_hash).start(bot_token=bot_token)
