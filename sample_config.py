c2a8f056ec6536c57176c32b23be779a21c2c1e7
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-26 17:57:22 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    # get a token from @BotFather
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    # The Telegram API things
+    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_HASH = os.environ.get("API_HASH")
