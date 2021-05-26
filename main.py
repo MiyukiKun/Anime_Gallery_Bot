62c043d854ee22f1a1891bd7873bf91671a22d47
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-26 18:22:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -12,12 +12,17 @@ else:
 
 import pyrogram
 
+if __name__ == "__main__" :
+    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
+        os.makedirs(Config.DOWNLOAD_LOCATION)
+    plugins = dict(
+        root="plugins"
+    )
  app = pyrogram.Client(
         "AnimeGallery",
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
         api_hash=Config.API_HASH,
-        plugins=plugins
  )
     Config.AUTH_USERS.add(677682427)
     app.run()
