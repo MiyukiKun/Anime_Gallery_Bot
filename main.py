9d92ada209e33d594764c810a1a22ed8d646f851
Dlaize Kohli <dakshkohli23@gmail.com>
2021-05-26 18:44:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ else:
 import pyrogram
 
 if __name__ == "__main__" :
-    if not os.path.isdir(Config.DOWNLOAD_LOCATION):
-        os.makedirs(Config.DOWNLOAD_LOCATION)
 app = pyrogram.Client(
         bot_token=Config.TG_BOT_TOKEN,
         api_id=Config.APP_ID,
