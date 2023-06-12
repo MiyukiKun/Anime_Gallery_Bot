d5d977dfe523c9aafa770afcce804b790cfb0fae
Majethia <hardikmajethia9529@gmail.com>
2023-06-12 14:54:46 +0530

    help text update

--- Helper/helper.py
+++ Helper/helper.py
@@ -9,15 +9,13 @@ help_text = '''List of commands:
 
 /batch <animeid>:<from episode>:<to episode> : For getting download links in bulk
 
-/download : same as batch download but you get 1 file you can import in **1DM** (here the highest possible resolution is provided so if you want to choose resolution use batch command)
+/download : same as batch download but you get 1 file you can import in **1DM** to download multiple files at once (here the highest possible resolution is provided so if you want to choose resolution use batch command)
 
 /manga <name of manga you want> : to read any manga by search
 
 /read <mangaid>:<ch num> : to read chapter
 
-/read_pdf <mangaid>:<ch num> : to export chapter as pdf
-
-/read_batch <mangaid>:<from chapter>:<to chapter> : For getting chapters in bulk
+/rbatch <mangaid>:<from chapter>:<to chapter> : For getting chapters in bulk
                 
 The **Download** links provided are in multiple qualities to download just open links in chrome and download starts automatically
 I suggest open the links in **1DM** app on playstore instead of chrome for easy downloading
